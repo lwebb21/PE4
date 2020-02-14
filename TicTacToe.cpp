246b2a321552be209e4fa3b99a5dd2d0c399b67d
Lucas Webb <lucasawebb@gmail.com>
2020-02-14 15:10:39 -0700

    Create TicTacToe.cpp
    Created the TicTacToe.cpp file and wrote the CreateBoard() method

---
+++ TicTacToe.cpp
@@ -0,0 +1,14 @@
+/**
+Lucas Webb, Rebecca Carr
+PE 4
+This program is a fun and exciting Tic Tac Toe game
+*/
+
+void CreateBoard(){
+    vector<vector<int> > board{{0, 0 , 0}, {0, 0 , 0}, {0, 0 ,0}};
+}
+
+int main(){
+    CreateBoard();
+    return 1;
+}
